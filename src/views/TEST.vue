59d4591c9e951b431dc6ca4603c80fdbab872d27
PathOfPain <dreamrunner891@gmail.com>
2024-08-19 04:08:21 +0300

    added profile with menus for different states, prescribed logic for VideoPopUp appearance

--- src/views/TEST.vue
+++ src/views/TEST.vue
@@ -1,13 +1,11 @@
 <script lang='ts' setup>
 import Footer from '@/components/Footer.vue';
-import VideoPopUp from '@/components/VideoPopUp.vue';
 import Preloader from '@/components/Preloader.vue';
 </script>
 
 <template>
   <div>
     <div class="test">
-      <VideoPopUp />
        <!-- <Preloader/> -->
     </div>
     <Footer/>
