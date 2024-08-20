09d32830c3c56c2e8d724619cc4ac12948913fbb
PathOfPain <dreamrunner891@gmail.com>
2024-08-20 22:10:21 +0300

    continue dev

---
+++ src/views/UserProfile/Setting.vue
@@ -0,0 +1,21 @@
+<script lang='ts' setup>
+
+</script>
+
+<template>
+  <div class="settings">
+    <div class="settings__container">
+      <div class="settings__avatar"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .settings__container {
+  max-width: 80%;
+  margin: 0 auto;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+</style>
